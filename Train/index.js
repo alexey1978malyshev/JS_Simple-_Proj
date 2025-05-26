47020028f0fa6523d4679ef3cbda4970ec272001
Alex <alexey@mail.ru>
2025-05-26 23:42:02 +0300

    app.js added

--- Train/index.js
+++ Train/index.js
@@ -11,48 +11,122 @@
 /*«Автор:» 
 Имя: Василий
 Фамилия: Васильев*/
-// 2
+// // 2
 
-const myName = 'Alex'
-const mySurname ='Malyshev'
-let myFavoriteDrink  = 'Coffee'
-let myFavoriteAnimal   = 'Urchin'
-let myFavoriteProgrammingLanguage = 'JS'
-console.log(`Меня зовут ${myName}`)
-console.log(mySurname)
-console.log(myFavoriteDrink)
-console.log(myFavoriteAnimal)
-console.log(myFavoriteProgrammingLanguage)
+// const myName = 'Alex'
+// const mySurname ='Malyshev'
+// let myFavoriteDrink  = 'Coffee'
+// let myFavoriteAnimal   = 'Urchin'
+// let myFavoriteProgrammingLanguage = 'JS'
+// console.log(`Меня зовут ${myName}`)
+// console.log(mySurname)
+// console.log(myFavoriteDrink)
+// console.log(myFavoriteAnimal)
+// console.log(myFavoriteProgrammingLanguage)
 
-//3
+// //3
 
-const carName = "Tesla"
-let maxCarSpeed  = 260
-let carOwner  = 'Mask Ilon'
-alert(carName)
-alert(maxCarSpeed)
-alert(carOwner)
+// const carName = "Tesla"
+// let maxCarSpeed  = 260
+// let carOwner  = 'Mask Ilon'
+// alert(carName)
+// alert(maxCarSpeed)
+// alert(carOwner)
 
-//4
-maxCarSpeed = 295
-console.log('У машины изменилась скорость', maxCarSpeed)
+// //4
+// maxCarSpeed = 295
+// console.log('У машины изменилась скорость', maxCarSpeed)
 
-carOwner = 'D_Tramp'
-console.log('У машины изменился владелец', carOwner)
+// carOwner = 'D_Tramp'
+// console.log('У машины изменился владелец', carOwner)
 
-//5
+// //5
 
-const question1='Какая самая маленькая страна мира по населению?'
+// const question1='Какая самая маленькая страна мира по населению?'
 
-const question2='Какая самая большая страна мира по населению?'
+// const question2='Какая самая большая страна мира по населению?'
 
-const question3='Какой самый большой материк на планете?ия'
+// const question3='Какой самый большой материк на планете?ия'
 
 
-const answer1 = 'Индия'
-const answer2 = 'Ватикан'
-const answer3 = 'Евразия'
+// const answer1 = 'Индия'
+// const answer2 = 'Ватикан'
+// const answer3 = 'Евразия'
 
-console.log(question1, 'Ответ: ',answer2 )
-console.log(question2, 'Ответ: ',answer1 )
-console.log(question3, 'Ответ: ',answer3 )
+// console.log(question1, 'Ответ: ',answer2 )
+// console.log(question2, 'Ответ: ',answer1 )
+// console.log(question3, 'Ответ: ',answer3 )
+// // 1
+// const firstname = "Alex";
+// console.log('firstname', firstname, typeof firstname);
+// const age = 22;
+// console.log('age', age, typeof age);
+// const isStudent = true;
+// console.log('isStudent', isStudent, typeof isStudent);
+// const wifeName = null;
+// console.log('wifeName', wifeName, typeof wifeName);
+// let x ;
+// console.log('x', x, typeof x);
+
+// const car = {
+//   name: "Toyota Corolla",
+//   year: 2024,
+//   isNew: false,
+//   owner: null
+// }
+// console.log('car', car, typeof car);
+// const id = Symbol("id");
+// console.log('id', id, typeof id);
+
+// const bigNumber = 123456789n;
+// console.log('bigNumber', bigNumber, typeof bigNumber)
+
+// // 2
+// let count = 20;
+// let view = "front";
+// let isCold = true;
+
+// count = 'двадцать';
+// alert(count);
+// view = 3;
+// alert(view);
+// isCold = null;
+// alert(isCold);
+
+// 3
+let ageOfPerson1 = 18;
+let ageOfPerson2 = '20';
+ageOfPerson2 = ageOfPerson1;
+console.log(ageOfPerson2, typeof ageOfPerson2)
+
+
+let nameOfAnimal1 = null;
+let nameOfAnimal2 = 'Charlie';
+nameOfAnimal2 = nameOfAnimal1;
+console.log(nameOfAnimal2, typeof nameOfAnimal2);
+
+const bestProgrammingLanguage1 = 'JavaScript';
+const bestProgrammingLanguage2 = 'Java';
+console.log('Переменную с const переопределить нельзя');
+console.log(bestProgrammingLanguage1, typeof bestProgrammingLanguage1);
+console.log(bestProgrammingLanguage2, typeof bestProgrammingLanguage2);
+
+let initialValue1;
+let initialValue2 = 0;
+initialValue2 = initialValue1;
+console.log(initialValue2, typeof initialValue2);
+
+let isJavaScriptProgrammer1 = true;
+let isJavaScriptProgrammer2 = false;
+console.log('var является устаревшим');
+isJavaScriptProgrammer2 = isJavaScriptProgrammer1;
+console.log(isJavaScriptProgrammer2, typeof isJavaScriptProgrammer2)
+
+let helloText1 = 'Hello!';
+let helloText2 = 'Привет!';
+helloText2 = helloText1;
+console.log(helloText2, typeof helloText2);
+
+
+
+
---
+++ slider/app.js
@@ -0,0 +1,7 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+sidebar.style.top =`-${2*100}vh`
